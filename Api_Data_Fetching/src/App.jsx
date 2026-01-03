cc572ee841409f635345fabf4acb5602e9af60e8
Abhishek Jaat <jaatabhishek61@gmail.com>
2026-01-03 16:53:59 +0530

    fix import path MyStore

--- Api_Data_Fetching/src/App.jsx
+++ Api_Data_Fetching/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { RouteProvide } from './My store/RouteProvide'
+import { RouteProvide } from './Mystore/RouteProvide'
 
 function App() {
   const [count, setCount] = useState(0)
